9ed5f15d7a7fc7a3d51615652a83a32ededfa8f9
GabeLops <gabrieldlops@gmail.com>
2020-01-08 22:53:38 -0800

    add UIImageView

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -9,5 +9,20 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    override func viewDidLoad() {
+           super.viewDidLoad()
+        title = "Picture"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
 
 }
