1f619ca5e20540aa6a3f2ea7a98442f4b319193a
GabeLops <gabrieldlops@gmail.com>
2020-01-09 23:42:12 -0800

    add picker controller function and filemanager

--- ViewController.swift
+++ ViewController.swift
@@ -37,18 +37,46 @@ UINavigationControllerDelegate {
         present(picker, animated: true)
     }
     
+    func getDocumentsDirectory() -> URL{
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else {return}
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        let person = Cell(caption: "LOL", fileName: "JO")
+        pictureArray.append(person)
+        imageLoaded()
+        tableView?.reloadData()
+        
+        dismiss(animated: true)
+    }
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictureArray.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? labelCell
+        let picture = pictureArray[indexPath.row]
+        cell?.name.text = (picture.caption)
         return cell!
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
      if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             let picture = pictureArray[indexPath.item]
+            vc.selectedImage = picture.caption
+            imageLoaded()
+            navigationController?.pushViewController(vc, animated: true)
             
             
         }
